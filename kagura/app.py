6c3b19d45447355605bcdf2ff02fcec0741655cb
yamamoto-tgz <yamamoto.tar.gz@gmail.com>
2025-08-25 10:22:38 +0900

    [Clean] 誤字修正

--- kagura/app.py
+++ kagura/app.py
@@ -5,16 +5,16 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-CROQUIS_DIR = "./kagura/static/img"
-CROQUIS_SIZE = 5
-CROQUIS_INTERVAL = 180
+KAGURA_DIR = "./kagura/static/img"
+KAGURA_SIZE = 5
+KAGURA_INTERVAL = 180
 
 
 @app.route("/")
 def index():
-    folders = sorted(os.listdir(CROQUIS_DIR))
+    folders = sorted(os.listdir(KAGURA_DIR))
     return render_template(
-        "index.html", size=CROQUIS_SIZE, interval=CROQUIS_INTERVAL, folders=folders
+        "index.html", size=KAGURA_SIZE, interval=KAGURA_INTERVAL, folders=folders
     )
 
 
@@ -23,7 +23,7 @@ def images():
     size = int(request.args.get("size"))
     folder = request.args.get("folder")
 
-    filenames = os.listdir(os.path.join(CROQUIS_DIR, folder))
+    filenames = os.listdir(os.path.join(KAGURA_DIR, folder))
     paths = [f"/static/img/{folder}/{filename}" for filename in filenames]
 
     random.shuffle(paths)
